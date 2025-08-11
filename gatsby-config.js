a8c1a6eb9a0eefee6f7af3ac2e8f5208060c7a64
손동주 <sondongju@sondongjuui-MacBookPro.local>
2025-08-11 16:27:48 +0900

    Remove problematic gatsby-plugin-gatsby-cloud and keep essential plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,32 +20,8 @@ module.exports = {
     social: {
       twitter: `aukkim`,
     },
-    email: `kimauk@kangwon.ac.kr`,
-    github: `https://github.com/aukkim`,
-    researchGate: `https://www.researchgate.net/profile/Auk-Kim`,
-    googleScholar: `https://scholar.google.com/citations?user=YOUR_GOOGLE_SCHOLAR_ID`,
-    location: `Kangwon National University
-College of Engineering 6, Room 512
-
-1 Gangwondaehakgil, Chuncheon-si, Gangwon-do (24341)
-Republic of Korea`,
-  
   },
   plugins: [
-    `gatsby-plugin-postcss`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `Inter\:300,400,500,600,700`,
-          `Noto Sans KR\:300,400,500,600,700`,
-          `Poppins\:300,400,500,600,700`,
-          `Source Sans Pro\:300,400,500,600,700`
-        ],
-        display: 'swap',
-      },
-    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -53,58 +29,34 @@ Republic of Korea`,
         name: `blog`,
       },
     },
-
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `members`,
-        path: `${__dirname}/content/members`,
-      },
-    },
-
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `publications`,
         path: `${__dirname}/content/publications`,
+        name: `publications`,
       },
     },
-
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `lectures`,
-        path: `${__dirname}/content/lectures`,
+        path: `${__dirname}/content/members`,
+        name: `members`,
       },
     },
-
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `news`,
         path: `${__dirname}/content/news`,
+        name: `news`,
       },
     },
-
-
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/content/lectures`,
+        name: `lectures`,
       },
     },
-
-
-
-    {
-      resolve: "gatsby-plugin-mdx",
-      options: {
-        extensions: [".mdx", ".md"],
-        // remark/rehype 플러그인도 여기 등록 가능
-      },
-    },
-    
     {
       resolve: `gatsby-transformer-remark`,
       options: {
@@ -130,55 +82,6 @@ Republic of Korea`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.nodes.map(node => {
-                return Object.assign({}, node.frontmatter, {
-                  description: node.excerpt,
-                  date: node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + node.fields.slug,
-                  custom_elements: [{ "content:encoded": node.html }],
-                })
-              })
-            },
-            query: `{
-              allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
-                nodes {
-                  excerpt
-                  html
-                  fields {
-                    slug
-                  }
-                  frontmatter {
-                    title
-                    date
-                  }
-                }
-              }
-            }`,
-            output: "/rss.xml",
-            title: "Gatsby Starter Blog RSS Feed",
-          },
-        ],
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Human-AI Interaction Lab`,
@@ -191,9 +94,5 @@ Republic of Korea`,
       },
     },
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
